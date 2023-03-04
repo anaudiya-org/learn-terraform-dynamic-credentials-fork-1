dac45b31ee67482080631423b755d60d62bc3118
amit-naudiyal <amit.naudiyal@yahoo.com>
2023-03-05 10:17:45 +1100

    Update variables.tf
    Changing region to SYD

--- aws/infra/variables.tf
+++ aws/infra/variables.tf
@@ -12,6 +12,6 @@ variable "tags" {
 
 variable "aws_region" {
   type        = string
-  default     = "us-east-2"
+  default     = "ap-southeast-2"
   description = "AWS region for all resources"
 }
