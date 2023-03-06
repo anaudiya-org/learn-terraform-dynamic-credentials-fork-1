cacdc6475774884a50bd677e3e00f8f59cd90d69
Amit Naudiyal <amit.naudiyal@rackspace.com>
2023-03-06 15:59:21 +1100

    adding more resources

--- aws/infra/instances.tf
+++ aws/infra/instances.tf
@@ -2,19 +2,38 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
   tags = merge(var.tags,  {
-    Name            = "ec2-tag-testing-instance"
+    Name            = "WEB-tag-testing-instance"
     a_Solution      = "TaggingSolution"
     a_Environment   = "uat01"
     Creator         = "amit.naudiyal"
   })
+
+  lifecycle {
+    ignore_changes = [
+      tags["CR_Ref"], tags["Opportunity_no"]
+    ]
+  }
+
+}
+
+resource "aws_instance" "cron" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name            = "CRON-tag-testing-instance"
+    a_Solution      = "TaggingSolution"
+    a_Environment   = "uat01"
+    Creator         = "amit.naudiyal"
+  }
+}
+
+resource "aws_s3_bucket" "tag-bucket" {
+  bucket = "tf-bucket-tagging-solution"
+
+  tags = {
+    Name            = "tf-bucket-tagging-solution"
+    a_Solution      = "TaggingSolution"
+  }
 }
